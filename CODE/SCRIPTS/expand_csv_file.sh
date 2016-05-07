4efe115c5401b50982802824d8d5d8ce6c4f81e7
Cat <Cat@catbook.media.mit.edu>
2016-05-07 17:32:23 -0400

    ignore case!!

--- CODE/SCRIPTS/expand_csv_file.sh
+++ CODE/SCRIPTS/expand_csv_file.sh
@@ -4,21 +4,16 @@
 # thus, you will have as many processes as lines in the file-- be cautious.
 # benchmark: 25 links in 2.830 seconds (9 links/sec)
 # 100 links in 13.262 seconds (7.7 links/sec)
-file="test.csv"
- 
+# usage: expand_csv_file.sh inputfile  
+
+file=$1
 
 while IFS=, read id url
 #while read line
 do 
   #outfile=$(echo $url | awk 'BEGIN { FS = "/" } ; {print $NF}')
-  ##echo $id  $url
-
-  curl -sIL $url | grep ^Location | tail -n1 | cut -c 11- &
-  #echo $id$url
-  #curl $url
-  #| grep ^Location  
-  #echo $id $url
-  #curl $url & 
+  curl -sIL $url | grep -i ^Location | tail -n1 | cut -c 11- &
+ 
 
 
 done  < "$file" 
