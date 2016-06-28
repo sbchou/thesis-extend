a13374c0d26160bf95315d9e1f11b4061caa6526
Cat <Cat@CatBook.local>
2016-06-27 23:50:23 -0400

    finish corr as counts

--- ANALYSIS/EmotVirality/clinton_corr.R
+++ ANALYSIS/EmotVirality/clinton_corr.R
@@ -1,87 +1,127 @@
+### Analyze Tweet Volume as a Count Distribution
+### Which it is.
 library(lme4)
 library(lmerTest)
+library(MASS)
+library(AER)
 
-# Let's repeat the correlations (and graph them!) with one camp at a time.
-
-### CLINTON ###
-stories <- read.table('../DATA/FOR_CORR/clinton_w_emot.csv', header=TRUE, sep = ",", quote ="" )
+stories <- read.table('../DATA/FOR_CORR/all_fields_no_title.csv', header=TRUE, sep = ",", quote = "'")
 nrow(stories)
 head(stories)
 
 # remove NaN for some reason the python code breaks the CSV
 stories <- stories[complete.cases(stories),]
 nrow(stories)
-# 1964
+# SHOULD BE 2651
+
+######## WC ###########
+### Model as Linear-- baseline ###
+model.lm.wc <- lm(num_tweets ~ wc, stories)
+summary(model.lm.wc)
+
+### Model as Poisson ###
+model.pois.wc = glm(num_tweets ~ wc, data=stories, family=poisson)
+summary(model.pois.wc)
+
+### Model as Negative Binomial ###
+model.nb.wc = glm.nb(num_tweets ~ wc, data=stories, control=glm.control(maxit=100))
+summary(model.nb.wc)
+
+# with log of data
+model.nb.logwc = glm.nb(num_tweets ~ log(wc), data=stories, control=glm.control(maxit=100))
+summary(model.nb.logwc)
+
+# goodness of fit
+1 - pchisq(summary(model.nb.logwc)$deviance,
+           summary(model.nb.logwc)$df.residual
+)
+
+### What model fits better? ###
+X0 <- 2 * (logLik(model.nb.wc) - logLik(model.lm.wc))
+X0 # 'log Lik.' -100852.6 (df=2)
+pchisq(X0, df = 0, lower.tail=FALSE)
+# 'log Lik.' 0 (df=3)
+
+X1 <- 2 * (logLik(model.nb.wc) - logLik(model.pois.wc))
+X1
+# 'log Lik.' 104529.3 (df=3)
+pchisq(X1, df = 0, lower.tail=FALSE)
+# 'log Lik.' 0 (df=3)
+# This very large chi-square strongly suggests the negative binomial model,
+# which estimates the dispersion parameter, is more appropriate than the Poisson model.
+
+# What fits better, log of x values or striaght up?
+Y <- 2 * (logLik(model.nb.logwc) - logLik(model.nb.wc))
+Y
+pchisq(Y, df=0, lower.tail=FALSE)
+# Log of data does! 13.97 Y p value 0
+
+# What performs better, linear model or nb model with log transform?
+test = 2*(logLik(model.nb.logwc) - logLik(lm(num_tweets ~ log(wc), data = stories)))
+test
+pchisq(test, df=0, lower.tail=FALSE)
+
+######## Emotionality ###########
+
+### Model as Linear-- baseline ###
+model.lm.emot <- lm(num_tweets ~ emotionality, stories)
+summary(model.lm.emot)
+
+### Model as Poisson ###
+model.pois.emot = glm(num_tweets ~ emotionality, data=stories, family=poisson)
+summary(model.pois.emot)
+
+### Model as Negative Binomial ###
+model.nb.emot = glm.nb(num_tweets ~ emotionality, data=stories)
+summary(model.nb.emot)
+
+# goodness of fit
+1 - pchisq(summary(model.nb.emot)$deviance,
+           summary(model.nb.emot)$df.residual
+)
+
+### What model fits better? ###
+X2 <- 2 * (logLik(model.nb.emot) - logLik(model.lm.emot))
+X2
+pchisq(X2, df = 0, lower.tail=FALSE)
+
+X3 <- 2 * (logLik(model.nb.emot) - logLik(model.pois.emot))
+X3
+# 'log Lik.' 104803.9 (df=3)
+pchisq(X3, df = 0, lower.tail=FALSE)
+# 'log Lik.' 0 (df=3)
+# This very large chi-square strongly suggests the negative binomial model,
+# which estimates the dispersion parameter, is more appropriate than the Poisson model.
 
 
-#[1] "url"             "org"             "candid_name"     "num_tweets"      "num_tweets_all"  "sentiment_mean" 
-#[7] "wc"              "negativ_ct"      "positiv_ct"      "negativ_percent" "positiv_percent" "emotionality"   
-#[13] "positivity"     
 
-######### Word Count ###############
-stories$num_tweets = as.numeric((stories$num_tweets))
-stories$wc = as.numeric((stories$wc))
-wc_corr <- lm(num_tweets ~ wc, stories)
-summary(wc_corr)
+######## Positivity ###########
+### Model as Linear-- baseline ###
+model.lm.pos <- lm(num_tweets ~ positivity, stories)
+summary(model.lm.pos)
 
-## log(WC) and Num Tweets 
-stories$log_wc = as.numeric((log(stories$wc)))
-log_wc_corr <- lm(num_tweets ~ log_wc, stories)
-summary(log_wc_corr)
+### Model as Poisson ###
+model.pois.pos = glm(num_tweets ~ positivity, data=stories, family=poisson)
+summary(model.pois.pos)
 
-## sqrt(WC) and Num Tweets 
-stories$sqrt_wc = as.numeric((sqrt(stories$wc)))
-sqrt_wc_corr <- lm(num_tweets ~ sqrt_wc, stories)
-summary(sqrt_wc_corr)
+### Model as Negative Binomial ###
+model.nb.pos = glm.nb(num_tweets ~ positivity, data=stories)
+summary(model.nb.pos)
 
-## log(WC) and log(Num Tweets)
-stories$log_num_tweets = as.numeric((log(stories$num_tweets)))
-loglog_wc_corr <- lm(log_num_tweets ~ log_wc, stories)
-summary(loglog_wc_corr)
+# goodness of fit
+1 - pchisq(summary(model.nb.pos)$deviance,
+           summary(model.nb.pos)$df.residual
+)
 
-plot(x = stories$log_wc, y=stories$log_num_tweets)
+### What model fits better? ###
+X4 <- 2 * (logLik(model.nb.pos) - logLik(model.lm.pos))
+pchisq(X4, df = 0, lower.tail=FALSE)
 
-######### Emotionality ###############
-## Plain
-emot_corr <- lm(num_tweets ~ emotionality, stories)
-summary(emot_corr)
-
-## log(emot) and Num Tweets 
-stories$log_emot = as.numeric((log(stories$emotionality + 1)))
-log_emot_corr <- lm(num_tweets ~ log_emot, stories)
-summary(log_emot_corr)
-
-## sqrt(emot) and Num Tweets 
-stories$sqrt_emot = as.numeric((sqrt(stories$emotionality)))
-sqrt_emot_corr <- lm(num_tweets ~ sqrt_emot, stories)
-summary(sqrt_emot_corr)
-
-## log(emot) and log(Num Tweets) 
-loglog_emot_corr <- lm(log_emot ~ log_wc, stories)
-summary(loglog_emot_corr)
-
-plot(x = stories$log_wc, y=stories$num_tweets)
-
-
-######### Positivity ###############
-## Plain
-pos_corr <- lm(num_tweets ~ positivity, stories)
-summary(pos_corr)
-
-## log(pos) and Num Tweets 
-stories$log_pos = as.numeric((log(stories$positivity+1)))
-log_pos_corr <- lm(num_tweets ~ log_pos, stories)
-summary(log_pos_corr)
-
-## sqrt(emot) and Num Tweets 
-stories$sqrt_emot = as.numeric((sqrt(stories$emotionality)))
-sqrt_emot_corr <- lm(num_tweets ~ sqrt_emot, stories)
-summary(sqrt_emot_corr)
-
-## log(emot) and log(Num Tweets) 
-loglog_emot_corr <- lm(log_emot ~ log_wc, stories)
-summary(loglog_emot_corr)
-
-plot(x = stories$log_wc, y=stories$num_tweets)
+X5 <- 2 * (logLik(model.nb.pos) - logLik(model.pois.pos))
+# 'log Lik.' 104946 (df=3)
+pchisq(X5, df = 0, lower.tail=FALSE)
+# 'log Lik.' 0 (df=3)
+# This very large chi-square strongly suggests the negative binomial model,
+# which estimates the dispersion parameter, is more appropriate than the Poisson model.
 
 
--- ANALYSIS/EmotVirality/poisson_neg_binomial.R
+++ ANALYSIS/EmotVirality/poisson_neg_binomial.R
@@ -1,6 +1,5 @@
 ### Analyze Tweet Volume as a Count Distribution
 ### Which it is.
-
 library(lme4)
 library(lmerTest)
 library(MASS)
@@ -16,6 +15,10 @@ nrow(stories)
 # SHOULD BE 2651
 
 ######## WC ###########
+### Model as Linear-- baseline ###
+model.lm.wc <- lm(num_tweets ~ wc, stories)
+summary(model.lm.wc)
+
 ### Model as Poisson ###
 model.pois.wc = glm(num_tweets ~ wc, data=stories, family=poisson)
 summary(model.pois.wc)
@@ -24,16 +27,46 @@ summary(model.pois.wc)
 model.nb.wc = glm.nb(num_tweets ~ wc, data=stories, control=glm.control(maxit=100))
 summary(model.nb.wc)
 
+# with log of data
+model.nb.logwc = glm.nb(num_tweets ~ log(wc), data=stories, control=glm.control(maxit=100))
+summary(model.nb.logwc)
+
+# goodness of fit
+1 - pchisq(summary(model.nb.logwc)$deviance,
+           summary(model.nb.logwc)$df.residual
+)
+
 ### What model fits better? ###
+X0 <- 2 * (logLik(model.nb.wc) - logLik(model.lm.wc))
+X0 # 'log Lik.' -100852.6 (df=2)
+pchisq(X0, df = 0, lower.tail=FALSE)
+# 'log Lik.' 0 (df=3)
+
 X1 <- 2 * (logLik(model.nb.wc) - logLik(model.pois.wc))
+X1
 # 'log Lik.' 104529.3 (df=3)
 pchisq(X1, df = 0, lower.tail=FALSE)
 # 'log Lik.' 0 (df=3)
 # This very large chi-square strongly suggests the negative binomial model,
 # which estimates the dispersion parameter, is more appropriate than the Poisson model.
 
+# What fits better, log of x values or striaght up?
+Y <- 2 * (logLik(model.nb.logwc) - logLik(model.nb.wc))
+Y
+pchisq(Y, df=0, lower.tail=FALSE)
+# Log of data does! 13.97 Y p value 0
+
+# What performs better, linear model or nb model with log transform?
+test = 2*(logLik(model.nb.logwc) - logLik(lm(num_tweets ~ log(wc), data = stories)))
+test
+pchisq(test, df=0, lower.tail=FALSE)
 
 ######## Emotionality ###########
+
+### Model as Linear-- baseline ###
+model.lm.emot <- lm(num_tweets ~ emotionality, stories)
+summary(model.lm.emot)
+
 ### Model as Poisson ###
 model.pois.emot = glm(num_tweets ~ emotionality, data=stories, family=poisson)
 summary(model.pois.emot)
@@ -41,17 +74,32 @@ summary(model.pois.emot)
 ### Model as Negative Binomial ###
 model.nb.emot = glm.nb(num_tweets ~ emotionality, data=stories)
 summary(model.nb.emot)
+ 
+# goodness of fit
+1 - pchisq(summary(model.nb.emot)$deviance,
+           summary(model.nb.emot)$df.residual
+)
 
 ### What model fits better? ###
-X2 <- 2 * (logLik(model.nb.emot) - logLik(model.pois.emot))
-# 'log Lik.' 104803.9 (df=3)
+X2 <- 2 * (logLik(model.nb.emot) - logLik(model.lm.emot))
+X2
 pchisq(X2, df = 0, lower.tail=FALSE)
+
+X3 <- 2 * (logLik(model.nb.emot) - logLik(model.pois.emot))
+X3
+# 'log Lik.' 104803.9 (df=3)
+pchisq(X3, df = 0, lower.tail=FALSE)
 # 'log Lik.' 0 (df=3)
 # This very large chi-square strongly suggests the negative binomial model,
 # which estimates the dispersion parameter, is more appropriate than the Poisson model.
 
 
+
 ######## Positivity ###########
+### Model as Linear-- baseline ###
+model.lm.pos <- lm(num_tweets ~ positivity, stories)
+summary(model.lm.pos)
+
 ### Model as Poisson ###
 model.pois.pos = glm(num_tweets ~ positivity, data=stories, family=poisson)
 summary(model.pois.pos)
@@ -60,14 +108,21 @@ summary(model.pois.pos)
 model.nb.pos = glm.nb(num_tweets ~ positivity, data=stories)
 summary(model.nb.pos)
 
+# goodness of fit
+1 - pchisq(summary(model.nb.pos)$deviance,
+           summary(model.nb.pos)$df.residual
+)
+
 ### What model fits better? ###
-X3 <- 2 * (logLik(model.nb.pos) - logLik(model.pois.pos))
+X4 <- 2 * (logLik(model.nb.pos) - logLik(model.lm.pos))
+pchisq(X4, df = 0, lower.tail=FALSE)
+
+X5 <- 2 * (logLik(model.nb.pos) - logLik(model.pois.pos))
 # 'log Lik.' 104946 (df=3)
-pchisq(X3, df = 0, lower.tail=FALSE)
+pchisq(X5, df = 0, lower.tail=FALSE)
 # 'log Lik.' 0 (df=3)
 # This very large chi-square strongly suggests the negative binomial model,
 # which estimates the dispersion parameter, is more appropriate than the Poisson model.
 
- 
 
  
